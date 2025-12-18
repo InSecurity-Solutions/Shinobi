38911c008a7b7a61f3d9794a9b93b46d9432068d
Moe <github@m03.ca>
2025-12-18 22:50:42 +0600

    Fix SSH bridge reconnect for Central Management

--- libs/connectToManagementServer/libs/centralConnect/ssh.js
+++ libs/connectToManagementServer/libs/centralConnect/ssh.js
@@ -46,7 +46,8 @@ function connectWebSocket() {
   ws.on('close', () => {
     console.log('Disconnected SSH from server');
     if (sshSocket) sshSocket.end();
-    scheduleReconnect();
+    // scheduleReconnect();
+    process.exit(0);
   });
 
   ws.on('error', (err) => {
